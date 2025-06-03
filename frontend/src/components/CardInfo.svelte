ea1e838fd9633bfdaa834fb9aedf45805bd66c7e
Chamini <bimalkachamini@gmail.com>
2025-06-03 17:23:37 +0530

    Refactor CardInfo component layout to a fixed position; enhance cart display with improved styling and add a "Pay" button alongside "Clear" button.

--- frontend/src/components/CardInfo.svelte
+++ frontend/src/components/CardInfo.svelte
@@ -4,20 +4,16 @@
     let { total, resetCart } = $props();
 </script>
 
-<section class="mt-10 bg-gray-50 p-6 rounded-md shadow-md">
-    <h2 class="text-2xl font-semibold mb-4">🛒 Your Cart</h2>
+<section class="fixed top-0 right-0 h-screen w-75 bg-gray-50 p-6 shadow-lg border-l border-gray-200 overflow-y-auto">
+    <h2 class="text-2xl font-semibold mb-4">Your Cart</h2>
 
     {#if $cart.length > 0}
         <div
             class="bg-white p-6 rounded-lg shadow-md border border-gray-200"
         >
-            <h2 class="text-xl font-semibold text-gray-800 mb-4">
-                🛒 Your Cart
-            </h2>
-
             <ul class="list-disc ml-6 space-y-2 text-gray-700">
                 {#each $cart as item}
-                    <li class="flex justify-between items-center">
+                    <li class="flex justify-between items-center bg-gray-200 p-2 rounded-md">
                         <span>{item.title}</span>
                         <span class="text-sm text-gray-500"
                             >× {item.quantity}</span>
@@ -37,10 +33,18 @@
         </div>
     {/if}
 
+    <div class="flex justify-between items-center">
     <button
-        class="mt-4 bg-red-500 hover:bg-red-600 text-white px-6 py-2 rounded-md transition-colors duration-200 flex items-center gap-2"
+        class="mt-4 w-1/3 bg-red-500 hover:bg-red-600 text-white px-6 py-2 rounded-md transition-colors duration-200 flex items-center justify-center gap-2"
         onclick={resetCart}
     >
-        Clear Cart 🧹
+        Clear
     </button>
+    <button
+        class="mt-4 w-1/3 bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md transition-colors duration-200 flex items-center justify-center gap-2"
+        onclick={resetCart}
+    >
+            Pay
+        </button>
+    </div>
 </section>
