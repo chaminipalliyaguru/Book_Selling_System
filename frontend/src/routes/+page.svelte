579b7a4de17a26eb809030d5d2a2b17a2f18e0c5
Chamini <bimalkachamini@gmail.com>
2025-06-04 23:50:37 +0530

    Enhance CardInfo component with payment integration; add Payment component for processing transactions and update layout for better user experience. Introduce FavoriteBook component and improve main page with favorites toggle functionality.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,14 +3,13 @@
     import { books, cart, fetchBooks } from "../lib/stores";
     import BookCard from "../components/BookCard.svelte";
     import { onMount } from "svelte";
-    //import { client, GET_BOOKS } from "../lib/graphql";
     import CardInfo from "../components/CardInfo.svelte";
 
-    //let bookList = $state([]);
     let bookList = $state($books); 
     let total = $state(0);
     let fetchedBooks = [];
     let showCart = $state(false);
+    let showFavorites = $state(false);
 
     onMount(async () => {
         await fetchBooks();
@@ -39,12 +38,19 @@
         📘 BookNest
     </h1>
 
+    <div class="flex gap-2 justify-end">
     <button onclick={() => showCart = !showCart} class="text-3xl">
        🛒 
     <span class="text-sm">{$cart.length}</span>
     </button>
-     
 
+    <button onclick={() => showFavorites = !showFavorites} style="width: 30px; height: 30px;">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="gray" stroke-width="2" viewBox="0 0 24 24" style="width: 100%; height: 100%;">
+		  <path stroke-linecap="round" stroke-linejoin="round" d="M3.172 5.172a4 4 0 015.656 0L12 8.343l3.172-3.171a4 4 0 115.656 5.656L12 21.343 3.172 10.828a4 4 0 010-5.656z"/>
+		</svg>
+	</button>
+    </div>
+     
     {#if showCart}
         <CardInfo {total} {resetCart} />
     {/if}
